bc3d05a5816196358d1aac31c795828014ed2c14
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-12-08 15:05:59 +0530

    Module exports

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { msg, calculateSum } = require("./sum");
+
 var name = "Mohit Agrawal";
 
 var a = 10;
@@ -5,3 +7,6 @@ var b = 20;
 
 console.log(name);
 console.log(a + b);
+
+console.log(msg);
+calculateSum(a, b);
---
+++ sum.js
@@ -0,0 +1,10 @@
+const msg = "Hello I am from sum.js";
+
+function calculateSum(a, b) {
+    console.log(a + b);
+}
+
+module.exports = {
+    msg,
+    calculateSum,
+};
